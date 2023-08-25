7ad0c8723fd75143a87ba8fcba5e6395d9f032a8
20120206 <nguyenngocthuy9a1@gmail.com>
2023-08-25 18:17:26 +0700

    other posts UI

--- src/api/httpRequest.js
+++ src/api/httpRequest.js
@@ -6,7 +6,6 @@ const httpRequest = axios.create({
 
 export const get = async (path, options = {}) => {
     const response = await httpRequest.get(path, options);
-    console.log(process.env.BASE_URL);
     return response.data;
 };
 
--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -1,7 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
-import { MdAddReaction, MdEmail } from 'react-icons/md';
+import { MdOutlineAddReaction } from 'react-icons/md';
 import styles from './Post.module.scss';
 import BlogBackground from '~/components/BlogBackground';
 import * as postService from '~/services/postService';
@@ -14,7 +14,10 @@ function Post() {
     const [otherPosts, setOtherPosts] = useState([]);
 
     useEffect(() => {
-        postService.getPosts(4).then((res) => setOtherPosts(res));
+        postService.getPosts(4).then((res) => {
+            const result = res.filter((otherPost) => otherPost.id !== post.id);
+            setOtherPosts(result);
+        });
     }, []);
 
     return (
@@ -27,21 +30,15 @@ function Post() {
                 <div className={cx('other-posts')}>
                     <h3>Other Posts</h3>
                     <div className={cx('posts-container')}>
-                        <div className={cx('other-post')}>
-                            <div className={cx('other-user')}>
-                                <img
-                                    className={cx('other-avatar')}
-                                    src="https://robohash.org/excepturiiuremolestiae.png"
-                                    alt=""
-                                />
-                                <h3 className={cx('other-name')}>Demetrius Corkery</h3>
+                        {otherPosts.map((item) => (
+                            <div key={item.id} className={cx('other-post')}>
+                                <p className={cx('other-title')}>{item.title}</p>
+                                <div className={cx('other-reaction')}>
+                                    <MdOutlineAddReaction />
+                                    <span className={cx('other-number-reaction')}>{item.reactions}</span>
+                                </div>
                             </div>
-                            <p>Title</p>
-                            <div className={cx('other-reaction')}>
-                                <MdAddReaction />
-                                <span className={cx('other-number-reaction')}>3</span>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </div>
